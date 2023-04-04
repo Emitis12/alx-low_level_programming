1fde3bd8cd6eb87a2fed8404eceabf2fda748492
Emitis12 <israeltim86@gmail.com>
2023-04-04 08:30:10 -0700

    strstr git commit -m strstr git commit -m strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystick: input
+ * @needle: input
+ * Retrun: Always 0 (Success)
+ */
+char *_strstr(char *haystick, char *needle)
+{
+	for (; *haystick != '\0'; haystick++)
+	{
+		char *one = haystick;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystick);
+	}
+
+	return (NULL);
+}
