57ec14787a17298ad554bc6c58455fc1fc5369d1
Emitis12 <israeltim86@gmail.com>
2023-04-06 05:45:48 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,18 +3,16 @@
 /**
  * _puts_recursion - Print a string followed by a new line
  * @s: the string to print
+ *
  * Return: Nothing.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-
+    if (*s == '\0') // base case: end of string
+    {
+        printf("\n"); // print new line
+        return;
+    }
+    putchar(*s); // print current character
+    _puts_recursion(s + 1); // call function recursively with next character
 }
